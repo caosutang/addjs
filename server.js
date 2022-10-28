c7b5eeed2f56364018c2c890b80f56e7fb28df2d
caosutang <minhhuyspkt@gmail.com>
2022-10-29 00:54:46 +0700

    Update 2

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var app = express();
 app.use(express.static(__dirname + "/public")); //__dir and not _dir
-var port = 8000; // you can use any port
-app.listen(port);
-console.log("server on" + port);
+const PORT = process.env.PORT || 80; // you can use any port
+app.listen(PORT);
+console.log("server on " + PORT);
